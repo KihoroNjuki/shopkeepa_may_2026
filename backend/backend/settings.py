cc1406c11149a366420bd064b7041dceb09d7292
Kihoro <victor.njuki@strathmore.edu>
2026-05-29 02:13:49 +0300

    New frontend URL

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -55,7 +55,7 @@ INSTALLED_APPS = [
 CORS_ALLOWED_ORIGINS = [
     'http://localhost:3000',
     'http://192.168.1.82:3000',
-    'https://stately-heliotrope-8b2566.netlify.app',
+    'https://shopkeepaapp.netlify.app',
 ]
 
 ALLOWED_HOSTS = ['*']
